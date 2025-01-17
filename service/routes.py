28f375b975e674420b4a5228fb2f8b24646829b5
SushC-OSU <sushc@okstate.edu>
2025-01-17 16:31:46 -0500

    Implemented list accounts functionality

--- service/routes.py
+++ service/routes.py
@@ -60,9 +60,17 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
-# ... place you code here to LIST accounts ...
-
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
@@ -85,7 +93,7 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-def update_account(account_id):
+def update_accounts(account_id):
     """
     Update an Account
     This endpoint will update an Account based on the posted data
